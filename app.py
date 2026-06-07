e599078568023a5a79c46ca102c52e138d21b300
otavioguilherme-dev <otavioguilherme@gmail.com>
2026-06-07 12:54:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 # Configuração visual da página (Modo centralizado tradicional)
 st.set_page_config(
     page_title="Agente de IA para Suporte Técnico e Instalação - OGNET-BORRACHAS",
-    page_icon="st.image("banner_ognet.png")",
+    page_icon="🔧",
     layout="centered"
 )
 
